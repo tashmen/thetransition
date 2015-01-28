502aa7fdb43e089f02ded6223ae7af1f7b459370
tashman <tashman@9ecdc47c-2d7f-4f8c-be3a-bc982e11d855>
2015-01-28 22:35:14 +0000

    PHPMailer - Can't use right now since SMTP ports are not open on the server due to free hosting limitations.

--- trunk/PHPServer/public_html/api.php
+++ trunk/PHPServer/public_html/api.php
@@ -2,6 +2,8 @@
 	require('/home/a1466265/public_html/Oauth2/Client.php'); 
 	require('/home/a1466265/public_html/Oauth2/GrantType/IGrantType.php'); 
 	require('/home/a1466265/public_html/Oauth2/GrantType/AuthorizationCode.php'); 
+	date_default_timezone_set('Etc/UTC');
+	//require('/home/a1466265/public_html/PHPMailer/PHPMailerAutoload.php');
 	
 	/*
 		Constants
@@ -53,13 +55,26 @@
 				$resource = $_REQUEST['resource'];
 				$action = $_REQUEST['action'];
 				
-				/*
-					Potential Security Implementation: Add login request as first request with userid and creation date; verify the data 
-					Then Add security checks based on the userid that is logged in.
-				*/
 				
 				if($resource == "" && $action == "")
 				{	
+					//Check if this is a cron job execution
+					/*
+					if($_REQUEST['cronjob'] == '1')
+					{
+						
+						$argv = array();
+						$argv[0] = 'test';
+						$argv[1] = 'automatedemail';
+						Logger::LogError("processing cron job from request", Logger::debug);
+					}
+					if (isset($argv)) {
+						Logger::LogError((extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n", Logger::debug);
+						$cronjob = new CronJob($argv);
+						$cronjob->ProcessJob();
+						Logger::LogError("processed cron job", Logger::debug);
+					}
+					*/
 					$nb = new NationBuilder();
 					$code = $_REQUEST['code'];
 					if($code != "")
@@ -137,6 +152,185 @@
 			}
 		}
 	}
+	/*
+	class CronJob
+	{
+		private $jobtype;
+		public function __construct($arguments)
+		{
+			$this->jobtype = $arguments[1];//arguments[0] point to this script
+		}
+		
+		/*
+			Processes the job based on the job type
+			Note job types are passed in via the cron job execution script which should look like this:
+			{php -f /home/a1466265/} public_html/api.php automatedemail
+			The above item in brackets is required and set automatically by the cron job UI that is available.
+		*/
+		public function ProcessJob()
+		{
+			if($this->jobtype == "automatedemail") 
+			{
+				$this->CheckAutomatedEmail();
+			}
+		}
+		
+		private function CheckAutomatedEmail()
+		{
+			//Query the autoresponderemails table using read()
+			//For each object see if any users fit the rule
+				//If rule is true
+					//Send email to user
+			
+			echo phpinfo();
+			
+			$fp = fsockopen('smtp.gmail.com', 587, $errno, $errstr, 5);
+			if (!$fp) {
+				echo "port is blocked";
+			} else {
+				// port is open and available
+				echo "Port is open";
+				fclose($fp);
+			}
+			
+			$fp = stream_socket_client("tcp://smtp.gmail.com:587", $errno, $errstr, 5);
+			if (!$fp) {
+				echo "$errstr ($errno)<br />\n";
+			} else {
+				while (!feof($fp)) {
+					echo fgets($fp, 1024);
+				}
+				fclose($fp);
+			}
+			/*
+			//Create a new SMTP instance 
+			$smtp = new SMTP; 
+			
+			 
+			//Enable connection-level debug output 
+			$smtp->do_debug = SMTP::DEBUG_CONNECTION; 
+			 
+			 
+			try { 
+				//Connect to an SMTP server 
+				if ($smtp->connect('smtp.gmail.com', 25)) { 
+					//Say hello 
+					if ($smtp->hello('thetransition.comeze.com')) { //Put your host name in here 
+						//Authenticate 
+						if ($smtp->authenticate('jonmnorcross@gmail.com', 'spanish2')) { 
+							echo "Connected ok!"; 
+						} else { 
+							throw new Exception('Authentication failed: ' . $smtp->getLastReply()); 
+						} 
+					} else { 
+						throw new Exception('HELO failed: '. $smtp->getLastReply()); 
+					} 
+				} 
+				else { 
+					throw new Exception('Connect failed'); 
+				} 
+			} catch (Exception $e) { 
+				echo 'SMTP error: '. $e->getMessage(), "\n"; 
+			} 
+			//Whatever happened, close the connection. 
+			$smtp->quit(true); 
+
+			/*
+			try
+			{
+				$email = new Email();
+				$email->CreateMessage("jonmnorcross@gmail.com", "Jon", "jonmnorcross@gmail.com", "Jon", "Test Email", "Test message, am I <b>bold</b>?<br><br>NO!");
+				$email->SendMessage();
+			} 
+			catch (phpmailerException $e) 
+			{
+				Logger::LogError($e->errorMessage(), Logger::fatalError);
+			}
+			*/
+		}
+	}
+	
+	class Email
+	{
+		private $phpmailer;
+		public function __construct()
+		{
+			//Create a new PHPMailer instance
+			$this->phpmailer = new PHPMailer();
+
+			//Tell PHPMailer to use SMTP
+			$this->phpmailer->isSMTP();
+
+			//Enable SMTP debugging
+			// 0 = off (for production use)
+			// 1 = client messages
+			// 2 = client and server messages
+			$this->phpmailer->SMTPDebug = 4;
+
+			//Ask for HTML-friendly debug output
+			$this->phpmailer->Debugoutput = 'html';
+
+			//Set the hostname of the mail server
+			$this->phpmailer->Host = 'smtp.gmail.com';
+
+			//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+			$this->phpmailer->Port = 587;
+
+			//Set the encryption system to use - ssl (deprecated) or tls
+			$this->phpmailer->SMTPSecure = 'tls';
+
+			//Whether to use SMTP authentication
+			$this->phpmailer->SMTPAuth = true;
+
+			//Username to use for SMTP authentication - use full email address for gmail
+			$this->phpmailer->Username = "jonmnorcross@gmail.com";
+
+			//Password to use for SMTP authentication
+			$this->phpmailer->Password = "spanish2";
+		}
+		
+		/*
+			Add the message attributes to the email
+			@param $fromAddress - the email address from where it originated
+			@param $fromNam - the name of the user where the email originates
+			@param $toAddress - the email address of the recipient
+			@param $toAddressName - the name of the recipient
+			@param $subject - the subject line of the email
+			@param $htmlMsg - the message to send in html format
+		*/
+		public function CreateMessage($fromAddress, $fromName, $toAddress, $toAddressName, $subject, $htmlMsg)
+		{
+			//Set who the message is to be sent from
+			$this->phpmailer->setFrom($fromAddress, $fromName);
+
+			//Set who the message is to be sent to
+			$this->phpmailer->addAddress($toAddress, $toAddressName);
+
+			//Set the subject line
+			$this->phpmailer->Subject = $subject;
+
+			//Read an HTML message body from an external file, convert referenced images to embedded,
+			//convert HTML into a basic plain-text alternative body
+			$this->phpmailer->msgHTML($htmlMsg);
+			
+			
+			/* If needed at later point adding our own alt text or attachments is posssible
+			//Replace the plain text body with one created manually
+			$mail->AltBody = 'This is a plain-text message body';
+
+			//Attach an image file
+			$mail->addAttachment('images/phpmailer_mini.png');
+			*/
+		}
+		
+		public function SendMessage()
+		{
+			//send the message, check for errors
+			if (!$this->phpmailer->send()) {
+				throw new Exception($this->phpmailer->ErrorInfo);
+			}
+		}
+	}*/
 	
 	class Security
 	{
