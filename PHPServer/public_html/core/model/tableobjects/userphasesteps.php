3ed132d634ed3b498574fefe496724d4ea6815ac
tashman <tashmen@users.noreply.github.com>
2015-03-30 22:06:52 +0000

    Add ability to autosync and fix a bug with completing steps and change default sort column to the number field.

--- PHPServer/public_html/core/model/tableobjects/userphasesteps.php
+++ PHPServer/public_html/core/model/tableobjects/userphasesteps.php
@@ -29,7 +29,7 @@ class userphasesteps extends TableObject{
      */
 
     protected function GetDefaultSortColumn(){
-        return 'userid';
+        return 'phasestepnumber';
     }
     
     /*
