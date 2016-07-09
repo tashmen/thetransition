5ae1afe299edfcade7575a9c4f12b7a39307c71b
tashman <tashman@9ecdc47c-2d7f-4f8c-be3a-bc982e11d855>
2016-07-09 20:25:47 +0000

    Add Organization signup page.

---
+++ trunk/PHPServer/public_html/core/model/tableobjects/userorgsignup.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Handles userorgsignup table object
+ *
+ * @author jnorcross
+ */
+class userorgsignup extends TableObject {
+    /*
+      Retrieves the primary table for the object
+      @return - the primary table as a string
+     */
+
+    public function GetPrimaryTable() {
+        return "userorgsignup";
+    }
+
+    /*
+      Retrieves the default sort column name
+      @return the full name of the sort column as a string to use as the default
+     */
+
+    protected function GetDefaultSortColumn() {
+        return 'name';
+    }
+    
+    /*
+      Retrieves whether or not the table has an auto-increment id.  These id's need to be handled and returned back to the user as they are generated so that update works properly.
+      Note: We assume that if this is true then the column name is 'id'
+      @return true if the table has this field otherwise false
+     */
+
+    protected function HasAutoIncrementId() {
+        return true;
+    }
+
+    /*
+     * Retrieves the security string for accessing the table object
+     * @return an array of allowed functions
+     */
+    public function GetSecurity(){
+        return array('read', 'create', 'update', 'delete');
+    }
+    
+    /*
+     * The list of events this object listens for
+     * @return - An array of events to listen for
+     */
+    public function GetEventListeners()
+    {
+        return array(EventMessenger::$OnDeleteUser);
+    }
+    
+    /*
+     * Deletes suggestions for the deleted user
+     * @param $parameters - Array of parameters
+     *  First parameter is the id of the user
+     */
+    public function OnDeleteUser($parameters)
+    {
+        $this->GetConnection()->execute("Delete from " . $this->GetPrimaryTable(). " where userid = ?", $parameters, false);
+    }
+}
